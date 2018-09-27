58843b30ef04dedbcdf4a24410a27879fd3fcc0b
BulldogX <qiu90123@163.com>
2018-09-27 21:47:02 +0800

    add components     1、prop-types     2、react-native-device-info     3、react-native-extra-dimensions-android     4、react-native-scrollable-tab-view

--- android/app/src/main/java/com/baishop/MainApplication.java
+++ android/app/src/main/java/com/baishop/MainApplication.java
@@ -3,6 +3,8 @@ package com.baishop;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ExtraDimensionsPackage(),
+            new RNDeviceInfo()
       );
     }
 
